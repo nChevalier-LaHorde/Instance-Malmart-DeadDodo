b3c7f55e1b33219ac8e35c29646e05d6e11058c6
Nova3324 <118186038+Nova3324@users.noreply.github.com>
2022-12-12 09:59:15 +0100

    map

---
+++ bin/assets/Floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Floor" tilewidth="32" tileheight="32" tilecount="1280" columns="15">
+ <image source="Room_Builder_Floors_32x32.png" width="480" height="1280"/>
+</tileset>
